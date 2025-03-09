92761af652581b2ff2e967f07d45437f7b981f30
BBhavya25 <bhavyabukya25@gmail.com>
2025-03-09 14:55:36 +0530

    Add files via upload

---
+++ fe/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">FOOD</div>
+          <div className="right">
+            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Open: 05:00 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODEZEESH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By ZEESH.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
